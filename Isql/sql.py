867ab94c4564bba7190db864331792e80da0b3f1
ajcltm <ajcltm@gmail.com>
2022-12-13 01:39:33 +0900

    primary key, autoincrement, indexing tool introduced.

--- Isql/sql.py
+++ Isql/sql.py
@@ -1,35 +1,35 @@
 from pydantic import BaseModel
 from datetime import datetime, date
 
-def stringfy(value:any)->str:
-    for_symbol = ["'", '(', ')', '%', '<', '>']
+def stringfy(value):
+
     if type(value) == str:
-        for i in for_symbol:
-            value = value.replace(i, '\\'+i)
-        value = value.replace('"', '""')
-        return f'"{value}"'
+        # symbols = ['\\', '"', "(", ")", "%", '&', '@', '*', '[', ']', '{', '}', '^', '!', '/', '-', '+', '?', ';', '~', '|']
+        # for symbol in symbols:
+        #     value = value.replace(symbol, '\\'+f'{symbol}')             
+        value = value.replace("'", "''") 
+        return f"'{value}'"
 
     elif type(value)==datetime :
-        value = value.strftime(format='%Y-%m-%d-%H-%M-%S')
-        return f'"{value}"'
+        value = value.strftime(format='%Y-%m-%d %H:%M:%S')
+        return f"'{value}'"
 
     elif type(value)==date :
         value = value.strftime(format='%Y-%m-%d')
-        return f'"{value}"'
+        return f"'{value}'"
 
     elif value == None:
-        return "Null"
+        return 'Null'
 
     else:
         return f'{value}'
 
-
 class TableIdentifier:
 
     def check_dataset_model(self, model):
-        if not list(model.__fields__.keys())[0] == 'dataset':
+        if not list(model.__fields__.keys())[0] == 'data':
             return False
-        if not model.__annotations__.get('dataset').__name__ == 'List':
+        if not model.__annotations__.get('data').__name__ == 'List':
             return False
         return True
 
@@ -37,7 +37,7 @@ class TableNameExporter(TableIdentifier):
 
     def get_table_name(self, model):
         if self.check_dataset_model(model):
-            table_name = model.__fields__.get('dataset').type_.__name__
+            table_name = model.__fields__.get('data').type_.__name__
         else:
             table_name= model.__name__
         return table_name
@@ -45,37 +45,118 @@ class TableNameExporter(TableIdentifier):
 class TableFieldsInfoExporter(TableIdentifier):
 
     def replace_type(self, model):
-        replace = {str:'text', int:'integer', float:'real', datetime:'date', date:'date'}
+        replace = {str:'text', int:'integer', float:'real', datetime:'datetime', date:'date'}
         return {key : replace.get(value.type_) for key, value in model.__fields__.items()}
 
     def get_fields_info(self, model):
         if self.check_dataset_model(model):
-            fields_info = self.replace_type(model=model.__fields__.get('dataset').type_) 
+            fields_info = self.replace_type(model=model.__fields__.get('data').type_) 
         else:
             fields_info = self.replace_type(model=model)
         return fields_info
 
 class CreateDropSql(BaseModel):
 
+    @staticmethod
+    def handle_duplicated_attr(**customType):
+        return {key.strip('_'):value for key, value in customType.items()}
+
+    @staticmethod
+    def add_property(fields_info, fields, property):
+        if isinstance(fields, list):
+            return {field : f'{fields_info.get(field)} {property}' for field in fields}
+        return {fields : f'{fields_info.get(fields)} {property}'}
+
     @classmethod
-    def get_create(cls, **customType):
+    def get_create(cls, autoIncrement=None, primaryKey=None, **customType):
         table_name= TableNameExporter().get_table_name(model=cls)
-        if not customType:
-            fields_info = TableFieldsInfoExporter().get_fields_info(model=cls)
-        else:
-            fields_info = customType
+        fields_info = TableFieldsInfoExporter().get_fields_info(model=cls)
+        customType=cls.handle_duplicated_attr(**customType)
+        fields_info.update(customType)
+        if autoIncrement:
+            autoIncrement_dict = cls.add_property(fields_info, autoIncrement, 'AUTO_INCREMENT')
+            fields_info.update(autoIncrement_dict)
         type_part = ', '.join([f'{field} {type_}' for field, type_ in fields_info.items()])
-        sql = f'CREATE TABLE IF NOT EXISTS {table_name} ({type_part})'
+        primaryKey = cls.handle_list(primaryKey)
+        if primaryKey:
+            sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({type_part}, PRIMARY KEY ({primaryKey}))"
+        else:
+            sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({type_part})"
         print(f'create sql : \n {sql}')
+        return sql
+    
+    @classmethod
+    def get_add_primaryKey(cls, primaryKey):
+        table_name= TableNameExporter().get_table_name(model=cls)
+        primaryKey = cls.handle_list(primaryKey)
+        sql = f'ALTER TABLE {table_name} ADD PRIMARY KEY ({primaryKey})'
+        print(f'primaryKey sql : \n {sql}')
+        print(f'Warning! : This method can be used only for MySql not Sqlite')
+        return sql
+    
+    @classmethod
+    def get_drop_primaryKey(cls):
+        table_name= TableNameExporter().get_table_name(model=cls)
+        sql = f'ALTER TABLE {table_name} DROP PRIMARY KEY'
+        print(f'primaryKey sql : \n {sql}')
+        print(f'Warning! : This method can be used only for MySql not Sqlite')
         return sql
 
     @classmethod
     def get_drop(cls):
         table_name = TableNameExporter().get_table_name(model=cls) 
         sql = f'DROP TABLE {table_name}'
-        print(f'create sql : \n {sql}')
+        print(f'drop sql : \n {sql}')
         return sql
     
+class IndexSql:
+
+    @staticmethod
+    def handle_list(value):
+        if isinstance(value, list):
+            return ', '.join(value)
+        return value
+
+    @staticmethod
+    def get_index_name(value):
+        if isinstance(value, list):
+            return 'index_'+'_'.join(value)
+        return f'index_{value}'
+
+    @classmethod
+    def get_create_index(cls, index, unique=None):
+        table_name= TableNameExporter().get_table_name(model=cls)
+        index_name = cls.get_index_name(index)
+        index = cls.handle_list(index)
+        if unique:
+            sql = f'CREATE UNIQUE INDEX IF NOT EXISTS {index_name} on {table_name} ({index})'
+        else:
+            sql = f'CREATE INDEX IF NOT EXISTS {index_name} on {table_name} ({index})'
+        print(f'index sql : \n {sql}')
+        return sql
+
+    @classmethod
+    def get_add_index(cls, index, unique=None):
+        table_name= TableNameExporter().get_table_name(model=cls)
+        index_name = cls.get_index_name(index)
+        index = cls.handle_list(index)
+        if unique:
+            sql = f'ALTER TABLE {table_name} ADD UNIQUE INDEX IF NOT EXISTS {index_name} ({index})'
+        else:
+            sql = f'ALTER TABLE {table_name} ADD INDEX IF NOT EXISTS {index_name} ({index})'
+        print(f'index sql : \n {sql}')
+        print(f'Warning! : This method can be used only for MySql not Sqlite')
+        return sql
+        
+    @classmethod
+    def get_drop_index(cls, index):
+        table_name= TableNameExporter().get_table_name(model=cls)
+        index_name = cls.get_index_name(index)
+        sql = f'DROP INDEX {index_name} ON {table_name}'
+        print(f'index sql : \n {sql}')
+        return sql
+
+
 class InsertSql:
 
     def get_values_part(self):
@@ -85,7 +166,7 @@ class InsertSql:
         return f'({values_part})'
 
     def get_values_parts(self):
-        values_part_lst = [data.get_values_part() for data in self.dataset]
+        values_part_lst = [data.get_values_part() for data in self.data]
         values_part = ', '.join(values_part_lst)
         return values_part
 
@@ -98,7 +179,7 @@ class InsertSql:
             values_part = self.get_values_part()
         fields_part = ', '.join(fields)
         sql = f'INSERT INTO {table_name} ({fields_part}) VALUES{values_part}'
-        print(f'insert sql : \n {sql}')
+        print(f'insert sql : \n {sql[:1000]}')
         return sql
 
 class DeleteSql:
@@ -137,5 +218,5 @@ class UpdateSql:
             print(f'update sql : \n {sql}')
             return sql
 
-class Sql(CreateDropSql, InsertSql, DeleteSql, UpdateSql):
+class Sql(CreateDropSql, IndexSql, InsertSql, DeleteSql, UpdateSql):
     pass
